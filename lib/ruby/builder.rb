06394b83e08e19f264f32e055e163a5c3a1bfb2b
Takashi Kokubun <takashikkbn@gmail.com>
2018-02-17 10:41:20 +0900

    bundle gem ruby-builder

---
+++ lib/ruby/builder.rb
@@ -0,0 +1,7 @@
+require "ruby/builder/version"
+
+module Ruby
+  module Builder
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/builder/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Builder
+    VERSION = "0.1.0"
+  end
+end
